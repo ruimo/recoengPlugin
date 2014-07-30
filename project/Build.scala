c04185b08bb4f017199fbb5562b832bf6f22886e
Shisei Hanai <ruimo.unog@gmail.com>
2014-07-31 05:59:37 +0900

    Use play 2.1

---
+++ project/Build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "recoengPlugin"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    publishTo := Some(
+      Resolver.file(
+        "recoeng",
+        new File(Option(System.getenv("RELEASE_DIR")).getOrElse("/tmp"))
+      )
+    )
+  )
+}
