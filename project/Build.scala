72dc10a9dfd53610664cce1a3f4b28e209f7560d
Shisei Hanai <ruimo.uno@gmail.com>
2014-07-31 08:28:39 +0900

    Set organization.

--- project/Build.scala
+++ project/Build.scala
@@ -14,10 +14,11 @@ object ApplicationBuild extends Build {
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
+    organization := "ruimo.com",
     publishTo := Some(
       Resolver.file(
         "recoeng",
-        new File(Option(System.getenv("RELEASE_DIR")).getOrElse("/tmp") + "/com/ruimo")
+        new File(Option(System.getenv("RELEASE_DIR")).getOrElse("/tmp"))
       )
     )
   )
